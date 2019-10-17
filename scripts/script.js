3d971088aba7eaedb3bf06d0f668f6d8357f0aaf
sharsheevnursultan <nursultan.sharsheev@gmail.com>
2019-10-17 22:59:45 +0600

    'initial_commit'

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+var input,search,pr,result,result_arr, locale_HTML, result_store;
+
+function func() {
+    locale_HTML = document.body.innerHTML;   // сохраняем в переменную весь body (Первоначальный)
+}
+setTimeout(func, 1000);  //ждем подгрузки Jsona и выполняем
+
+function FindOnPage(name, status) {
+
+    input = document.getElementById(name).value; //получаем значение из поля в html
+
+    if(input.length<3&&status==true)
+    {
+        alert('Для поиска вы должны ввести три или более символов');
+        function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
+    }
+
+    if(input.length>=3)
+    {
+        function FindOnPageGo() {
+
+            search = '/'+input+'/g';  //делаем из строки регуярное выражение
+            pr = document.body.innerHTML;   // сохраняем в переменную весь body
+            result = pr.match(/>(.*?)</g);  //отсекаем все теги и получаем только текст
+            result_arr = [];   //в этом массиве будем хранить результат работы (подсветку)
+
+            var warning = true;
+            for(var i=0;i<result.length;i++) {
+                if(result[i].match(eval(search))!=null) {
+                    warning = false;
+                }
+            }
+            if(warning == true) {
+                alert('Не найдено ни одного совпадения');
+            }
+
+            for(var i=0; i<result.length;i++) {
+                result_arr[i] = result[i].replace(eval(search), '<span style="background-color:yellow;">'+input+'</span>'); //находим нужные элементы, задаем стиль и сохраняем в новый массив
+            }
+            for(var i=0; i<result.length;i++) {
+                pr=pr.replace(result[i],result_arr[i])  //заменяем в переменной с html текст на новый из новогом ассива
+            }
+            document.body.innerHTML = pr;  //заменяем html код
+        }
+    }
+    function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
+    if(status) { FindOnPageBack(); FindOnPageGo(); } //чистим прошлое и Выделяем найденное
+    if(!status) { FindOnPageBack(); } //Снимаем выделение
+}
